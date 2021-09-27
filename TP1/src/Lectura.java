dc2ea4e939e5a4b89d15fb54b1664e253cfa682a
JoaquinFioriti <70446526+JoaquinFioriti@users.noreply.github.com>
2021-09-27 15:17:45 -0300

    Calculando la entropia de una fuente markoviana ergodica

--- TP1/src/Lectura.java
+++ TP1/src/Lectura.java
@@ -146,4 +146,17 @@ public class Lectura {
         return result;
     }
 
+
+    public double calculaEntropiaMarkoviana(double[] vec, double[][] matriz){
+        double acum1=0, acum2=0;
+        for (int i = 0; i < vec.length; i++){
+            acum2=0;
+            for (int j = 0; j< matriz[i].length; j++){
+                acum2 += matriz[j][i] * (-Math.log(matriz[j][i]) / Math.log(2));
+            }
+            acum1 += vec[i] * acum2;
+        }
+        return acum1;
+    }
+
 }
